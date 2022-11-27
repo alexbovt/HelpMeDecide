be90e54079ecfb93fbf960939bc5b01fed141c9f
alexbovt <bovttt@gmail.com>
2022-11-27 15:51:59 +0100

    add precommit hooks, add eslint, prettier

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+    flex: 1,
+    background-color: '#fff',
+    alig-items: 'center',
+    justify-content: 'center',
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Container>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
